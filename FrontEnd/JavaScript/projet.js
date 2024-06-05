04bf719e49d641348155a7e4621034e8413c59ef
Noa <noa.houssin@hotmail.com>
2024-06-05 23:18:47 +0200

    Restructuration du code et et fin du filtrage des boutons par catégorie

--- FrontEnd/JavaScript/projet.js
+++ FrontEnd/JavaScript/projet.js
@@ -1,3 +1,7 @@
+// Sélectionner l'élément de la galerie
+const gallery = document.querySelector(".gallery");
+displayWorks();
+displayCategories();
 // Fonction pour récupérer les données de l'API
 async function fetchWorks() {
   try {
@@ -15,32 +19,98 @@ async function fetchWorks() {
   }
 }
 
-// Fonction pour ajouter dynamiquement les projets à la page
 async function displayWorks() {
+  const works = await fetchWorks();
+  works.forEach((work) => {
+    createWorks(work);
+  });
+}
+
+// Fonction pour ajouter dynamiquement les projets à la page
+function createWorks(work) {
+  // Sélectionner l'élément de la galerie
+  const gallery = document.querySelector(".gallery");
+  
+  // Création l'élément figure
+  const figure = document.createElement("figure");
+  figure.dataset.categoryId = work.categoryId;
+  // Création l'élément img et définir sa source
+  const imageProjet = document.createElement("img");
+  imageProjet.src = work.imageUrl;
+  //   Création de l'élément figcaption et définition du texte
+  const figcaption = document.createElement("figcaption");
+  figcaption.innerText = work.title;
+  // Ajouter img et figcaption à figure
+  figure.appendChild(imageProjet);
+  figure.appendChild(figcaption);
+  // Ajouter figure à la galerie
+  gallery.appendChild(figure);
+}
+
+// Fonction pour récupérer les données de l'API
+async function fetchCategories() {
   try {
-    const works = await fetchWorks();
-      // Sélectionner l'élément de la galerie
-    const gallery = document.querySelector(".gallery");
-      // Boucler à travers chaque projet pour créer les éléments HTML
-    for (let i = 0; i < works.length; i++) {
-        // Création l'élément figure 
-      const figure = document.createElement("figure");
-       // Création l'élément img et définir sa source
-      const imageProjet = document.createElement("img");
-      imageProjet.src = works[i].imageUrl;
-    //   Création de l'élément figcaption et définition du texte
-      const figcaption = document.createElement("figcaption");
-      figcaption.innerText = works[i].title;
-        // Ajouter img et figcaption à figure
-      figure.appendChild(imageProjet);
-      figure.appendChild(figcaption);
-       // Ajouter figure à la galerie
-      gallery.appendChild(figure);
+    const response = await fetch("http://localhost:5678/api/categories");
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
     }
+    // Convertir la réponse en JSON
+    return await response.json();
     // gestion des erreurs
   } catch (error) {
-    console.error("Erreur lors de l'affichage des projets:", error);
+    console.error("Erreur lors de la récupération des categories:", error);
+    throw error; // Rethrow pour permettre la gestion de l'erreur dans la fonction displayCategories
   }
 }
 
-displayWorks();
+// Fonction pour ajouter dynamiquement les projets à la page
+async function displayCategories() {
+  try {
+    const categories = await fetchCategories();
+    console.log(categories);
+    const btnTous = {
+      id: "",
+      name: "Tous",
+    };
+
+    categories.unshift(btnTous);
+    // Sélectionner l'élément de la galerie
+    const filters = document.getElementById("filters");
+    // Boucler à travers chaque catégorie pour créer les éléments HTML
+    categories?.forEach((category) => {
+      // Création d'un élément bouton
+      let btn = document.createElement("button");
+      // ajout du nom des différentes catégories dans ce bouton
+      btn.innerText = category.name;
+      // Ajout de l'id correspondant à chaque catégorie
+      btn.dataset.categoryId = category.id;
+      console.log(category);
+      btn.addEventListener("click", filterProject);
+      // ajout de l'élément bouton dans la div filters
+      filters.appendChild(btn);
+    });
+    // gestion des erreurs
+  } catch (error) {
+    console.error("Erreur lors de l'affichage des catégories:", error);
+  }
+}
+
+async function filterProject(event) {
+  console.log(event.target);
+  const filterWorks = await fetchWorks();
+  gallery.innerHTML=" ";  
+  let categoryTargetId = event.target.dataset.categoryId;
+  
+  console.log(categoryTargetId);
+  if (categoryTargetId !== "") {
+    filterTriWorks = filterWorks.filter((work) => {
+      return work.categoryId == categoryTargetId;
+    });
+    console.log(filterTriWorks)
+    filterTriWorks.forEach((work) => {
+      createWorks(work);
+    });
+  } else {
+    displayWorks();
+  }
+}
