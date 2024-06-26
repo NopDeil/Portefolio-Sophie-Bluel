3600c532a465b53b4641a581723445352cc4da10
Noa <noa.houssin@hotmail.com>
2024-06-26 18:21:53 +0200

    Fix error validator + fix mise en en forme css

--- FrontEnd/JavaScript/connexion.js
+++ FrontEnd/JavaScript/connexion.js
@@ -1,31 +1,55 @@
 // Récupération des éléments
-const form = document.getElementById("form");
+const form = document.getElementById("form"); // Sélectionne le formulaire par son ID
 
+// Ajout d'un écouteur d'événement sur la soumission du formulaire
 form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  form_verify();
+  e.preventDefault(); // Empêche le comportement par défaut de soumission du formulaire
+  form_verify(); // Appelle la fonction de vérification du formulaire
 });
 
+// Fonction asynchrone pour vérifier les champs du formulaire et envoyer une requête de connexion
 async function form_verify() {
-  let error = document.querySelector(".error");
-  let email = document.getElementById("email");
-  let password = document.getElementById("password");
+  let error = document.querySelector(".error"); // Sélectionne l'élément d'erreur pour afficher les messages d'erreur
+  let email = document.getElementById("email"); // Sélectionne le champ email par son ID
+  let password = document.getElementById("password"); // Sélectionne le champ mot de passe par son ID
+
+  // Vérifie si les champs email et mot de passe sont vides
+  if (!email.value && !password.value) {
+    email.classList.add("inputError"); // Ajoute une classe d'erreur au champ email
+    password.classList.add("inputError"); // Ajoute une classe d'erreur au champ mot de passe
+    error.innerHTML = "Merci de renseigner un email et un mot de passe"; // Affiche un message d'erreur
+    return; // Sort de la fonction
+  } else if (!email.value && password.value) {
+    email.classList.add("inputError"); // Ajoute une classe d'erreur au champ email
+    error.innerHTML = "Merci de renseigner un email"; // Affiche un message d'erreur
+    return; // Sort de la fonction
+  } else if (email.value && !password.value) {
+    password.classList.add("inputError"); // Ajoute une classe d'erreur au champ mot de passe
+    error.innerHTML = "Merci de renseigner un mot de passe"; // Affiche un message d'erreur
+    return; // Sort de la fonction
+  }
+
+  // Envoie une requête POST à l'API pour vérifier les informations de connexion
   let req = await fetch("http://localhost:5678/api/users/login", {
     method: "POST",
     headers: {
-      "Content-Type": "application/json",
+      "Content-Type": "application/json", // Définit le type de contenu comme JSON
     },
-    body: JSON.stringify({ email: email.value, password: password.value }),
+    body: JSON.stringify({ email: email.value, password: password.value }), // Convertit les données du formulaire en JSON pour les envoyer
   });
+
+  // Vérifie si la réponse n'a pas le statut 200 (OK)
   if (req.status !== 200) {
-    email.classList.add("inputError");
-    password.classList.add("inputError");
-    error.innerHTML = "L’identifiant ou le mot de passe n'est pas valide";
-    return;
+    email.classList.add("inputError"); // Ajoute une classe d'erreur au champ email
+    password.classList.add("inputError"); // Ajoute une classe d'erreur au champ mot de passe
+    error.innerHTML = "L’identifiant ou le mot de passe n'est pas valide"; // Affiche un message d'erreur
+    return; // Sort de la fonction
   }
+
+  // Si la réponse a le statut 200 (OK)
   if (req.status === 200) {
-    let res = await req.json();
-    localStorage.setItem("token", res.token);
-    window.location.href = "http://127.0.0.1:5500/FrontEnd/";
+    let res = await req.json(); // Convertit la réponse en JSON
+    localStorage.setItem("token", res.token); // Stocke le token dans le stockage local
+    window.location.href = "./index.html"; // Redirige l'utilisateur vers la page d'accueil
   }
 }
--- FrontEnd/JavaScript/projet.js
+++ FrontEnd/JavaScript/projet.js
@@ -1,11 +1,13 @@
 // Sélectionner l'élément de la galerie
 const gallery = document.querySelector(".gallery");
 const worksModal = document.querySelector(".worksModal");
+
+// Appeler les fonctions pour afficher les travaux et les catégories
 displayWorks();
 displayCategories();
-
 connected();
-// Fonction pour récupérer les données de l'API
+
+// Fonction pour récupérer les données de l'API des travaux
 async function fetchWorks() {
   try {
     const response = await fetch("http://localhost:5678/api/works");
@@ -15,41 +17,44 @@ async function fetchWorks() {
     // Convertir la réponse en JSON
     const works = await response.json();
     return works;
-    // gestion des erreurs
   } catch (error) {
     console.error("Erreur lors de la récupération des projets:", error);
-    throw error; // Rethrow pour permettre la gestion de l'erreur dans la fonction displayWorks
+    throw error; // Relancer l'erreur pour la gestion dans la fonction displayWorks
   }
 }
 
+// Fonction pour afficher les travaux dans la galerie
 async function displayWorks() {
-  const works = await fetchWorks();
+  gallery.innerHTML = ""; // Effacer le contenu actuel de la galerie
+  const works = await fetchWorks(); // Récupérer les travaux
   works.forEach((work) => {
-    createWorks(work);
+    createWorks(work); // Créer et ajouter chaque travail à la galerie
   });
 }
 
 // Fonction pour ajouter dynamiquement les projets à la page
 function createWorks(work) {
-  // Sélectionner l'élément de la galerie
-
-  // Création l'élément figure
+  // Création de l'élément figure
   const figure = document.createElement("figure");
   figure.dataset.categoryId = work.categoryId;
-  // Création l'élément img et définir sa source
+
+  // Création de l'élément img et définition de sa source
   const imageProjet = document.createElement("img");
   imageProjet.src = work.imageUrl;
-  //   Création de l'élément figcaption et définition du texte
+
+  // Création de l'élément figcaption et définition du texte
   const figcaption = document.createElement("figcaption");
   figcaption.innerText = work.title;
+
   // Ajouter img et figcaption à figure
   figure.appendChild(imageProjet);
   figure.appendChild(figcaption);
+
   // Ajouter figure à la galerie
   gallery.appendChild(figure);
 }
 
-// Fonction pour récupérer les données de l'API
+// Fonction pour récupérer les données de l'API des catégories
 async function fetchCategories() {
   try {
     const response = await fetch("http://localhost:5678/api/categories");
@@ -58,14 +63,13 @@ async function fetchCategories() {
     }
     // Convertir la réponse en JSON
     return await response.json();
-    // gestion des erreurs
   } catch (error) {
     console.error("Erreur lors de la récupération des categories:", error);
-    throw error; // Rethrow pour permettre la gestion de l'erreur dans la fonction displayCategories
+    throw error; // Relancer l'erreur pour la gestion dans la fonction displayCategories
   }
 }
 
-// Fonction pour ajouter dynamiquement les projets à la page
+// Fonction pour afficher dynamiquement les catégories sur la page
 async function displayCategories() {
   if (!localStorage.token) {
     try {
@@ -75,10 +79,11 @@ async function displayCategories() {
         id: "",
         name: "Tous",
       };
+      categories.unshift(btnTous); // Ajouter un bouton "Tous" au début des catégories
 
-      categories.unshift(btnTous);
-      // Sélectionner l'élément de la galerie
+      // Sélectionner l'élément des filtres
       const filters = document.getElementById("filters");
+
       // Boucler à travers chaque catégorie pour créer les éléments HTML
       categories?.forEach((category, index) => {
         // Création d'un élément bouton
@@ -86,28 +91,26 @@ async function displayCategories() {
         if (index === 0) {
           btn.classList.add("button-active");
         }
-        // ajout du nom des différentes catégories dans ce bouton
-        btn.innerText = category.name;
-        // Ajout de l'id correspondant à chaque catégorie
-        btn.dataset.categoryId = category.id;
+        btn.innerText = category.name; // ajout du nom des différentes catégories dans ce bouton
+        btn.dataset.categoryId = category.id; // Ajout de l'id correspondant à chaque catégorie
         console.log(category);
-        btn.addEventListener("click", filterProject);
-        // ajout de l'élément bouton dans la div filters
-        filters.appendChild(btn);
+        btn.addEventListener("click", filterProject); // Ajouter un écouteur d'événement pour filtrer les projets
+        filters.appendChild(btn); // Ajouter le bouton dans la div filters
       });
-      // gestion des erreurs
     } catch (error) {
       console.error("Erreur lors de l'affichage des catégories:", error);
     }
   }
 }
 
+// Fonction pour filtrer les projets par catégorie
 async function filterProject(event) {
   console.log(event.target);
   const filterWorks = await fetchWorks();
   gallery.innerHTML = "";
   let categoryTargetId = event.target.dataset.categoryId;
 
+  // Mettre à jour l'état des boutons
   document.querySelectorAll("#portfolio button").forEach((element) => {
     element.classList.remove("button-active");
   });
@@ -115,19 +118,20 @@ async function filterProject(event) {
 
   console.log(categoryTargetId);
   if (categoryTargetId !== "") {
+    // Filtrer les travaux par catégorie
     filterTriWorks = filterWorks.filter((work) => {
       return work.categoryId == categoryTargetId;
     });
     console.log(filterTriWorks);
     filterTriWorks.forEach((work) => {
-      createWorks(work);
+      createWorks(work); // Ajouter chaque travail filtré à la galerie
     });
   } else {
-    displayWorks();
+    displayWorks(); // Afficher tous les travaux
   }
 }
 
-// Après connexion de l'utilisateur
+// Fonction appelée après la connexion de l'utilisateur
 function connected() {
   const logout = document.querySelector(".logout");
   const token = localStorage.getItem("token");
@@ -136,10 +140,12 @@ function connected() {
     logout.innerText = "logout";
     logout.href = "";
     logout.addEventListener("click", (e) => {
-      e.preventDefault;
-      token = localStorage.removeItem("token");
-      window.location.href = "../index.html";
+      e.preventDefault();
+      localStorage.removeItem("token");
+      window.location.href = "./index.html";
     });
+
+    // Activer le mode édition
     const mode = document.querySelector(".mode");
     mode.classList.add("edition");
     const square1 = document.querySelector(".mode p i");
@@ -156,6 +162,39 @@ function connected() {
     spanTextModal.textContent = "Modifier";
     const containerModals = document.querySelector(".containerModals");
     const btnAjoutPhoto = document.querySelector(".modalWorks button");
+
+    displayWorksModal();
+
+    const fileBtn = document.querySelector(".photoModal .button");
+    const fileInput = document.getElementById("fileInput");
+    const photoModal = document.querySelector(".photoModal");
+    const iconImg = document.querySelector(".fa-image");
+    const btnImg = document.querySelector(".photoModal button");
+    const pFile = document.querySelector(".photoModal p");
+    const imgFile = document.createElement("img");
+    const formPhotoModal = document.querySelector(".formPhotoModal");
+
+    // Affichage de l'image après l'envoi d'un fichier
+    fileBtn.addEventListener("click", () => {
+      fileInput.click();
+      fileInput.addEventListener("change", () => {
+        const files = fileInput.files;
+        const file = files[0];
+        if (file) {
+          const reader = new FileReader();
+          reader.onload = (e) => {
+            imgFile.src = e.target.result;
+            imgFile.classList.add("imgFile");
+            imgFile.style.display = "flex";
+            iconImg.style.display = "none";
+            btnImg.style.display = "none";
+            pFile.style.display = "none";
+          };
+          reader.readAsDataURL(file);
+        }
+        photoModal.appendChild(imgFile);
+      });
+    });
 
     pModal.addEventListener("click", () => {
       containerModals.style.display = "block";
@@ -175,9 +214,11 @@ function connected() {
     btnAjoutPhoto.addEventListener("click", () => {
       containerModals.style.display = "none";
       containerAddPhotoModals.style.display = "block";
+      iconImg.style.display = "block";
+      btnImg.style.display = "block";
+      pFile.style.display = "block";
+      formPhotoModal.reset();
     });
-
-    displayWorksModal();
 
     const containerAddPhotoModals = document.querySelector(
       ".containerAddPhotoModals"
@@ -192,53 +233,29 @@ function connected() {
     leftArrow.addEventListener("click", () => {
       containerModals.style.display = "block";
       containerAddPhotoModals.style.display = "none";
+      imgFile.style.display = "none";
+      formPhotoModal.reset();
     });
 
     xmarks1.addEventListener("click", () => {
       containerAddPhotoModals.style.display = "none";
+      imgFile.style.display = "none";
+      formPhotoModal.reset();
     });
 
     containerAddPhotoModals.addEventListener("click", (e) => {
       if (e.target.className == "containerAddPhotoModals") {
         containerAddPhotoModals.style.display = "none";
+        imgFile.style.display = "none";
+        formPhotoModal.reset();
       }
-      const fileBtn = document.querySelector(".photoModal .button");
-      const fileInput = document.getElementById("fileInput");
-      const photoModal = document.querySelector(".photoModal");
-      const iconImg = document.querySelector(".fa-image");
-      const btnImg = document.querySelector(".photoModal button");
-      const pFile = document.querySelector(".photoModal p");
-      const imgFile = document.createElement("img");
-
-      // Affichage de l'image après l'envoie d'un fichier
-
-      fileBtn.addEventListener("click", () => {
-        fileInput.click();
-        fileInput.addEventListener("change", () => {
-          const files = fileInput.files;
-          const file = files[0];
-          if (file) {
-            const reader = new FileReader();
-
-            reader.onload = (e) => {
-              imgFile.src = e.target.result;
-              imgFile.classList.add("imgFile");
-              imgFile.style.display = "flex";
-              iconImg.style.display = "none";
-              btnImg.style.display = "none";
-              pFile.style.display = "none";
-            };
-            reader.readAsDataURL(file);
-          }
-          photoModal.appendChild(imgFile);
-        });
-      });
     });
-    ValiderForm()
+
     postWorks();
   }
 }
 
+// Fonction pour afficher les travaux dans la modale
 async function displayWorksModal() {
   worksModal.innerHTML = "";
   const works = await fetchWorks();
@@ -249,37 +266,21 @@ async function displayWorksModal() {
   deleteWorks();
 }
 
+// Fonction pour créer les éléments de travaux dans la modale
 function createWorksModal(work) {
-  // Création l'élément figure
+  // Création de l'élément figure
   const figure = document.createElement("figure");
-  // Création l'élément img, span et i
+  // Création de l'élément img, span et i
   const image = document.createElement("img");
   const span = document.createElement("span");
   const suppr = document.createElement("i");
   suppr.classList.add("fa-solid", "fa-trash-can");
-  suppr.id = work.id;
+  suppr.dataset.id = work.id;
   image.src = work.imageUrl;
   span.appendChild(suppr);
   figure.appendChild(span);
   figure.appendChild(image);
   worksModal.appendChild(figure);
-}
-
-
-// Fonction pour valider le formulaire de la modale
-function ValiderForm () {
-  const formPhotoModal = document.querySelector(".formPhotoModal");
-  const titleForm = document.querySelector(".formPhotoModal #titre");
-  const fileInput = document.getElementById("fileInput");
-  const categoryForm = document.querySelector(".formPhotoModal #category");
-  const btnValid = document.querySelector(".formPhotoModal button");
-
-  formPhotoModal.addEventListener("input", () => {
-    if (fileInput.value !== "" && titleForm.value !== "" && categoryForm.value !== "") {
-      btnValid.classList.remove("disabled");
-      btnValid.disabled = true;  
-    } 
-  });
 }
 
 // Fonction pour ajouter un projet via la modale
@@ -306,11 +307,11 @@ function postWorks() {
         },
         body: formData,
       });
-      
+
       if (response.status !== 201) {
         throw new Error("HTTP error! Status: " + response.status);
       }
-      
+
       if (response.status === 201) {
         console.log("Ce projet à été ajouté !");
         displayWorksModal();
@@ -322,14 +323,80 @@ function postWorks() {
   });
 }
 
-// fonction pour supprimer un projet dans la modale
+// Fonction pour valider le formulaire de la modale
+function validerForm() {
+  const formPhotoModal = document.querySelector(".formPhotoModal");
+  const titleForm = document.querySelector(".formPhotoModal #titre");
+  const fileInput = document.getElementById("fileInput");
+  const categoryForm = document.querySelector(".formPhotoModal #category");
+  const btnValid = document.querySelector(".formPhotoModal button");
+
+  formPhotoModal.addEventListener("input", () => {
+    if (
+      !fileInput.value &&
+      !titleForm.value &&
+      !categoryForm.value
+    ){
+      btnValid.classList.add("disabled");
+      btnValid.disabled = true;
+      return;
+    }else if(fileInput.value &&
+      !titleForm.value &&
+      !categoryForm.value){
+        btnValid.classList.add("disabled");
+        btnValid.disabled = true;
+        return;
+      }else if(!fileInput.value &&
+      titleForm.value &&
+      !categoryForm.value){
+        btnValid.classList.add("disabled");
+        btnValid.disabled = true;
+        return;
+      }else if(!fileInput.value &&
+        !titleForm.value &&
+        categoryForm.value){
+          btnValid.disabled = true;
+          btnValid.classList.add("disabled");
+          return;
+        }else if(fileInput.value &&
+          titleForm.value &&
+          !categoryForm.value){
+            btnValid.disabled = true;
+            btnValid.classList.add("disabled");
+            return;
+          }else if(fileInput.value &&
+            !titleForm.value &&
+            categoryForm.value){
+              btnValid.disabled = true;
+              btnValid.classList.add("disabled");
+              return;
+            }else if(!fileInput.value &&
+              titleForm.value &&
+              categoryForm.value){
+                btnValid.classList.add("disabled");
+                btnValid.disabled = true;
+                return;
+              }else if(fileInput.value &&
+                titleForm.value &&
+                categoryForm.value){
+                  btnValid.classList.remove("disabled");
+                  btnValid.disabled = false;
+                  return;
+                }
+      
+  });
+}
+
+// Fonction pour supprimer un projet dans la modale
 function deleteWorks() {
   const trashCan = document.querySelectorAll(".fa-trash-can");
   console.log(trashCan);
   const token = localStorage.getItem("token");
   trashCan.forEach((trash) => {
     trash.addEventListener("click", async (e) => {
-      const id = trash.id;
+      e.preventDefault();
+      const id = trash.dataset.id;
+      console.log(id);
       try {
         const response = await fetch(`http://localhost:5678/api/works/${id}`, {
           method: "DELETE",
@@ -337,22 +404,17 @@ function deleteWorks() {
             Authorization: "Bearer " + token,
           },
         });
-        // Convertir la réponse en JSON
         if (response.status !== 204) {
           throw new Error(`HTTP error! Status: ${response.status}`);
         }
         if (response.status === 204) {
-          e.preventDefault();
           console.log("Ce projet à été supprimé !");
           displayWorksModal();
           displayWorks();
         }
-
-        // gestion des erreurs
       } catch (error) {
         console.error("Erreur lors de la suppression du projet:", error);
       }
     });
   });
 }
-
